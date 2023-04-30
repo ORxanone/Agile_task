ae053704fd3e0a6782f5bdc2079974ea49174935
ORxanone <recebovorxan3@gmail.com>
2023-04-30 22:24:12 +0400

    agile

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+var a = document.getElementById("closemenu");
+var b = document.getElementById("nav");
+
+
+function myFunction(){
+    
+    b.style.left = '0px'    
+}
+
+function rClose(){
+    b.style.left = "-400px"
+}
+
